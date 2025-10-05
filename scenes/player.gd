03fd63d2b41cd00adfa5063033c3a133df94901f
Ib3600 <Ibrahim2005elouzzani@gmail.com>
2025-10-05 14:30:29 +0200

    Initial commit

---
+++ scenes/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+var is_flipped:bool
+var is_attacking:bool
+@export var moveSpeed:int
+
+signal attacking
+
+
+func _ready():
+	is_flipped = true
+	attacking.connect(attack)
+	pass
+	
+func _process(delta):
+	input()
+	
+func input():
+	if is_attacking : 
+		attack()
+	else :
+		var direction = Input.get_vector("left", "right", "up", "down")
+		velocity = direction.normalized() * moveSpeed
+		if (direction != Vector2(0,0)):
+			$AnimatedSprite2D.play("run")
+		else : 
+			$AnimatedSprite2D.play("idle")
+		
+		if (direction.x > 0):
+			if is_flipped == true : 
+				$AnimatedSprite2D.flip_h = false
+				is_flipped = false
+		elif (direction.x < 0):
+			if is_flipped == false : 
+				$AnimatedSprite2D.flip_h = true
+				is_flipped = true
+		move_and_slide()
+	
+func attack():
+	is_attacking = true
+	
+	pass
